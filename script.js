0d3ecb35fe941d590b0fe48732bcb4eb84cb894a
Kinga Woch <kinga-niecko@o2.pl>
2023-04-15 13:23:33 +0200

    js

---
+++ script.js
@@ -0,0 +1,61 @@
+const form = document.querySelector("form");
+const input = document.querySelector("#link-input");
+const errorMsg = document.querySelector(".error-msg");
+const linksContainer = document.querySelector(".links-container");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputURL = input.value;
+  if (inputURL === "") {
+    input.classList.add("invalid");
+    errorMsg.classList.add("visible");
+  } else {
+    input.classList.remove("invalid");
+    errorMsg.classList.remove("visible");
+  }
+  input.value = "";
+  callAPI(inputURL);
+});
+
+let apiURL = "https://api.shrtco.de/v2/shorten?url=";
+
+const callAPI = (inputURL) => {
+  fetch(`${apiURL + inputURL}`)
+    .then((res) => res.json())
+    .then((data) => {
+      //console.log(data);
+      if (data.ok === true) {
+        renderOutput(inputURL, data.result.full_short_link);
+      } else {
+        input.classList.add("invalid");
+      }
+    });
+};
+
+const renderOutput = (input, shortenUrl) => {
+  const link = `
+    <li class="link">
+        <span class="original-link">https://${input}</span>
+        <div class="result">
+          <span class="result-link">${shortenUrl}</span>
+          <button class="copy-btn">Copy</button>
+        </div>
+    </li>
+    `;
+  linksContainer.insertAdjacentHTML("afterbegin", link);
+
+  let resultLink = document.querySelector(".result-link").innerHTML;
+  const copyBtn = document.querySelector(".copy-btn");
+
+  const copyLink = async () => {
+    try {
+      await navigator.clipboard.writeText(resultLink);
+      console.log("content copied");
+      copyBtn.innerText = "Copied!";
+      copyBtn.classList.add("copied");
+    } catch (err) {
+      console.error("failed", err);
+    }
+  };
+  copyBtn.addEventListener("click", copyLink);
+};
